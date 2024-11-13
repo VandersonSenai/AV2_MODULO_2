be765c111f22252b922921ae0821959d25b61d7c
VandersonSenai <vanderson.silva13@aluno.senai.br>
2024-11-13 19:20:20 -0300

    Pergunta_02

---
+++ Pergunta_03/src/main/java/com/mycompany/pergunta_03/Pergunta_03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pergunta_03;
+
+/**
+ *
+ * @author v.silva
+ */
+public class Pergunta_03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
